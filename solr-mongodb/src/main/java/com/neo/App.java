5995426fa474c7fa68e7b601b927bb4290112b5b
neowyp <neowyp@10.144.52.120>
2015-04-14 14:51:18 +0800

    create solr mongodb project

---
+++ solr-mongodb/src/main/java/com/neo/App.java
@@ -0,0 +1,13 @@
+package com.neo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
