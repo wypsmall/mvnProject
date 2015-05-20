8117a0bf69b76a0752af6803bd416eb641f2dcd6
neowyp <neowyp@10.144.48.10>
2015-05-20 13:59:17 +0800

    JmTest java project not maven project

---
+++ JmTest/src/neo/ce/JmeterVirtual.java
@@ -0,0 +1,61 @@
+package neo.ce;
+
+import neo.ce.service.IVirtualService;
+
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JmeterVirtual extends AbstractJavaSamplerClient {
+	private static String label_name = "dubbo_consumer";
+	private static final ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
+			new String[] { "spring-context-dubbo.xml" }); 
+	private static IVirtualService virtualService = null;
+	
+	
+	@Override
+	public Arguments getDefaultParameters() {
+		Arguments params = new Arguments();       
+        params.addArgument("name", "Tom");
+        return params;
+	}
+
+
+	@Override
+	public void setupTest(JavaSamplerContext context) {
+//		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] { "spring-context-dubbo.xml" });
+		virtualService = (IVirtualService) ctx.getBean("virtualService");
+	}
+
+
+	@Override
+	public void teardownTest(JavaSamplerContext context) {
+		super.teardownTest(context);
+	}
+
+
+	@Override
+	public SampleResult runTest(JavaSamplerContext arg0) {
+		boolean success = true;
+        SampleResult sr = new SampleResult();
+        sr.setSampleLabel(label_name);
+        sr.sampleStart();//用来统计执行时间--start--
+        try {
+            String name = arg0.getParameter("name");
+            String msg = virtualService.hello(name);
+            //Thread.sleep(5000);
+            System.out.println(msg);
+            sr.setResponseMessage(msg);
+            sr.setResponseCode("1000");
+        } catch (Exception e) {
+            success = false;
+        }finally{
+            sr.sampleEnd();//用来统计执行时间--end--
+            sr.setSuccessful(success);
+        }
+        return sr;
+	}
+
+}
---
+++ JmTest/src/neo/ce/MyJmeter.java
@@ -0,0 +1,48 @@
+package neo.ce;
+
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+public class MyJmeter extends AbstractJavaSamplerClient {
+
+	@Override
+	public Arguments getDefaultParameters() {
+		Arguments params = new Arguments();       
+        params.addArgument("name", "Tom");
+        return params;
+	}
+
+	@Override
+	public void setupTest(JavaSamplerContext context) {
+		super.setupTest(context);
+	}
+
+	@Override
+	public void teardownTest(JavaSamplerContext context) {
+		super.teardownTest(context);
+	}
+
+	@Override
+	public SampleResult runTest(JavaSamplerContext arg0) {
+		boolean success = true;
+        SampleResult sr = new SampleResult();
+        sr.setSampleLabel("MyJmeter");
+        sr.sampleStart();//用来统计执行时间--start--
+        try {
+            String name = arg0.getParameter("name");
+            Thread.sleep(5000);
+            System.out.println(name);
+            sr.setResponseMessage(name);
+            sr.setResponseCode("1000");
+        } catch (Exception e) {
+            success = false;
+        }finally{
+            sr.sampleEnd();//用来统计执行时间--end--
+            sr.setSuccessful(success);
+        }
+        return sr;
+	}
+
+}
